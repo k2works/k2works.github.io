928e2e9fb60c4461a3a3f530955c15073f45f308
k2works <kakimomokkuri@gmail.com>
2021-06-26 11:22:36 +0900

    test: マネー

--- docs/slides/20210630/main_test.py
+++ docs/slides/20210630/main_test.py
@@ -1,7 +1,6 @@
 
 import unittest
 
-
 class Money:
     def __init__(self, amount, currency) -> None:
         self.__amount = amount
@@ -13,7 +12,8 @@ class Money:
 class TestMoney(unittest.TestCase):
     def test_金額を表示する(self):
         千円 = Money(1000, 'JPY')
+        一万円 = Money(10000, 'JPY')
         self.assertEqual(str(千円), '¥1000')
-
+        self.assertEqual(str(一万円), '¥10000')
 
 unittest.main(argv=[''], verbosity=2, exit=False)
