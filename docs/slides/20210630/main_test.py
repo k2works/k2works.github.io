2009f8908ca1f4bff15ae759263a2dbe1110c85d
k2works <kakimomokkuri@gmail.com>
2021-06-26 11:21:36 +0900

    test: マネー

--- docs/slides/20210630/main_test.py
+++ docs/slides/20210630/main_test.py
@@ -2,6 +2,14 @@
 import unittest
 
 
+class Money:
+    def __init__(self, amount, currency) -> None:
+        self.__amount = amount
+        self.__currency = currency
+
+    def __str__(self) -> str:
+        return '¥1000'
+
 class TestMoney(unittest.TestCase):
     def test_金額を表示する(self):
         千円 = Money(1000, 'JPY')
