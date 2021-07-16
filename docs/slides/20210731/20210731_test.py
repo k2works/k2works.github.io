964a7f4bfc45d33dc8a99d2aa54d818c4d56be9c
k2works <kakimomokkuri@gmail.com>
2021-07-16 19:20:27 +0900

    test: 20210731

--- docs/slides/20210731/20210731_test.py
+++ docs/slides/20210731/20210731_test.py
@@ -8,30 +8,54 @@ Base = declarative_base()
 
 
 class TestUser(unittest.TestCase):
+    def setUp(self) -> None:
+        self.name = Name(first='ルー', last='大柴')
+
     def test_名前を登録できる(self):
-        user = User(name='ユーザ名')
-        self.assertEqual(user.name, 'ユーザ名')
+        user = User(self.name)
+        self.assertEqual(user.name, 'ルー 大柴')
 
     def test_住所を登録できる(self):
-        user = User(name='ユーザー名', zip='123-4567', address='住所')
+        user = User(self.name, zip='123-4567', address='住所')
         self.assertEqual(user.zip, '123-4567')
         self.assertEqual(user.address, '住所')
 
 
 class TestRepository(unittest.TestCase):
+    def setUp(self) -> None:
+        self.name = Name(first='高木', last='ブー')
+
     def test_ユーザを登録できる(self):
-        user = User(name='ユーザ名', zip='123-4567', address='住所')
+        user = User(self.name, zip='123-4567', address='住所')
         repo = Repository()
         repo.save(user)
         result = repo.find()
-        self.assertEqual(result.name, 'ユーザ名')
+        self.assertEqual(result.name, '高木 ブー')
 
     def test_ユーザを検索できる(self):
-        user = User(name='ユーザ名', zip='123-4567', address='住所')
+        user = User(self.name, zip='123-4567', address='住所')
         repo = Repository()
         repo.save(user)
-        result = repo.find_by_name('ユーザ名')
-        self.assertEqual(result.name, 'ユーザ名')
+        result = repo.find_by_name(str(self.name))
+        self.assertEqual(result.name, '高木 ブー')
+
+
+class Name:
+    def __init__(self, first, last):
+        self.__first = first
+        self.__last = last
+
+    @property
+    def first(self):
+        return self.__first
+
+    @property
+    def last(self):
+        return self.__last
+
+    def __str__(self):
+        return '{} {}'.format(self.__first, self.__last)
+
 
 
 class User(Base):
@@ -40,6 +64,12 @@ class User(Base):
     name = Column(String)
     zip = Column(String)
     address = Column(String)
+
+    def __init__(self, name, zip=None, address=None) -> None:
+        super().__init__()
+        self.name = str(name)
+        self.zip = zip
+        self.address = address
 
 
 class Repository:
