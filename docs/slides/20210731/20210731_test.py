bdbdcf90a8af86968a1bd53c22ead7dbb8a8edbc
k2works <kakimomokkuri@gmail.com>
2021-07-16 17:40:28 +0900

    test: 20210731

--- docs/slides/20210731/20210731_test.py
+++ docs/slides/20210731/20210731_test.py
@@ -1,5 +1,10 @@
 import unittest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float
 
+Base = declarative_base()
 
 class TestUser(unittest.TestCase):
     def test_名前を登録できる(self):
@@ -12,7 +17,22 @@ class TestUser(unittest.TestCase):
         self.assertEqual(user.address, '住所')
 
 
-class User:
+class TestRepository(unittest.TestCase):
+    def test_ユーザを登録できる(self):
+        user = User(name='ユーザ名', zip='123-4567', address='住所')
+        repo = Repository()
+        repo.save(user)
+        result = repo.find()
+        self.assertEqual(result.name, 'ユーザ名')
+
+
+class User(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    zip = Column(String)
+    address = Column(String)
+    __tablename__ = 'users'
+
     def __init__(self, name, zip=None, address=None) -> None:
         self.__name = name
         self.__zip = zip
@@ -29,6 +49,20 @@ class User:
     @property
     def address(self):
         return self.__address
+
+
+class Repository:
+    def __init__(self) -> None:
+        self.engine = create_engine('sqlite:///:memory:')
+        self.session = sessionmaker(bind=self.engine)()
+        self.conn = self.engine.connect()
+        Base.metadata.create_all(self.engine)
+
+    def save(self, user):
+        self.session.add(user)
+
+    def find(self):
+        return self.session.query(User).first()
 
 
 unittest.main(argv=[''], verbosity=2, exit=False)
