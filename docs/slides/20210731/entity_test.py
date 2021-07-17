8405ac5f4ba43f0ff6b07392b27c7f377ec10f08
k2works <kakimomokkuri@gmail.com>
2021-07-17 16:52:33 +0900

    test: エンティティ

--- docs/slides/20210731/entity_test.py
+++ docs/slides/20210731/entity_test.py
@@ -1,4 +1,10 @@
 import unittest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+Base = declarative_base()
 
 
 class TestUser(unittest.TestCase):
@@ -11,25 +17,21 @@ class TestUser(unittest.TestCase):
     def test_住所を登録できる(self):
         self.assertEqual(self.user.address, "733-0000 広島県広島市西区横川町1-2-3")
 
+
 class TestRepository(unittest.TestCase):
     def test_ユーザを登録できる(self):
         user = User(name="高木 ブー", address="733-0000 広島県広島市西区横川町1-2-3")
-        repo = Repository()
+        repo = SQLiteRepositry()
         repo.add(user)
         self.assertEqual(repo.get(user.name), user)
 
-class User:
-    def __init__(self, name, address):
-        self.__name = name
-        self.__address = address
 
-    @property
-    def name(self):
-        return self.__name
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    address = Column(String)
 
-    @property
-    def address(self):
-        return self.__address
 
 class Repository:
     def __init__(self):
@@ -40,3 +42,17 @@ class Repository:
 
     def get(self, name):
         return self.__users[name]
+
+
+class SQLiteRepositry:
+    def __init__(self):
+        self.engine = create_engine('sqlite:///:memory:')
+        self.session = sessionmaker(bind=self.engine)()
+        self.conn = self.engine.connect()
+        Base.metadata.create_all(self.engine)
+
+    def add(self, user):
+        self.session.add(user)
+
+    def get(self, name):
+        return self.session.query(User).filter_by(name=name).first()
