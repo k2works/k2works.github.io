828572b4a2390195a9fe1ac0705ebd0db8d0d650
k2works <kakimomokkuri@gmail.com>
2021-07-19 10:34:11 +0900

    test: エンティティ

--- docs/slides/20210731/entity_test.py
+++ docs/slides/20210731/entity_test.py
@@ -1,135 +1,55 @@
 import unittest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from abc import ABCMeta, abstractmethod
-from enum import Enum
 
 Base = declarative_base()
 
 
 class TestUser(unittest.TestCase):
-    def setUp(self) -> None:
-        self.user = User(name=Name(first="高木", last="ブー"),
-                         address=Address(postal_code="733-0000", prefecture="広島県", city="広島市西区", town="横川町1-2-3"), role=Role.管理者)
-
-    def test_名前を登録できる(self):
-        self.assertEqual(str(self.user.name), "高木 ブー")
-
-    def test_住所を登録できる(self):
-        self.assertEqual(str(self.user.address), "733-0000 広島県広島市西区横川町1-2-3")
-
-    def test_役割を登録できる(self):
-        self.assertEqual(self.user.role, Role.管理者)
+    def test_user(self):
+        user = User()
+        user.name = '柿木 勝之'
+        user.address = '722-001 広島県広島市横川町1-2-3 123'
+        self.assertEqual(user.name, '柿木 勝之')
 
 
 class TestRepository(unittest.TestCase):
-    def test_ユーザを登録できる(self):
-        user = User(name=Name(first="高木", last="ブー"),
-                    address=Address(postal_code="733-0000", prefecture="広島県", city="広島市西区", town="横川町1-2-3"))
-        repo = SQLiteRepositry()
+    def test_repository(self):
+        user = User()
+        user.name = '柿木 勝之'
+        user.address = '722-001 広島県広島市横川町1-2-3 123'
+        repo = SQLiteRepository()
         repo.add(user)
-        self.assertEqual(repo.get(user.name), user)
-
-
-class Name:
-    def __init__(self, first, last):
-        self.__first = first
-        self.__last = last
-
-    def __str__(self) -> str:
-        return "{} {}".format(self.__first, self.__last)
-
-    @property
-    def first(self):
-        return self.__first
-
-    @property
-    def last(self):
-        return self.__last
-
-
-class Address:
-    def __init__(self, postal_code, prefecture, city, town):
-        self.__postal_code = postal_code
-        self.__prefecture = prefecture
-        self.__city = city
-        self.__town = town
-
-    @property
-    def postal_code(self):
-        return self.__postal_code
-
-    @property
-    def prefecture(self):
-        return self.__prefecture
-
-    @property
-    def city(self):
-        return self.__city
-
-    @property
-    def town(self):
-        return self.__town
-
-    def __str__(self) -> str:
-        return "{} {}{}{}".format(self.__postal_code, self.__prefecture, self.__city, self.__town)
-
-
-class Role(Enum):
-    管理者 = 1
-    利用者 = 2
+        self.assertEqual(repo.get(1).name, '柿木 勝之')
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    postal_code = Column(String)
-    prefecture = Column(String)
-    city = Column(String)
-    town = Column(String)
-    role_no = Column(Integer)
+    name = Column(String)
+    address = Column(String)
 
-    def __init__(self, name: Name, address: str, role: str = Role.利用者):
-        super().__init__()
-        self.first_name = name.first
-        self.last_name = name.last
-        self.postal_code = address.postal_code
-        self.prefecture = address.prefecture
-        self.city = address.city
-        self.town = address.town
-        self.role_no = role.value
-
-    @property
-    def name(self) -> Name:
-        return Name(self.first_name, self.last_name)
-
-    @property
-    def address(self) -> Address:
-        return Address(self.postal_code, self.prefecture, self.city, self.town)
-
-    @property
-    def role(self) -> Role:
-        return Role(self.role_no)
-
-
-class Repository(metaclass=ABCMeta):
-    @abstractmethod
-    def add(self, user):
-        pass
-
-    @abstractmethod
-    def get(self, name):
-        pass
-
-
-class SQLiteRepositry(Repository):
     def __init__(self):
         super().__init__()
-        self.engine = create_engine('sqlite:///:memory:')
+
+
+class Repository:
+    def __init__(self):
+        self.users = []
+
+    def add(self, user):
+        self.users.append(user)
+
+    def get(self, index):
+        return self.users[index]
+
+
+class SQLiteRepository:
+    def __init__(self):
+        self.engine = create_engine('sqlite:///:memory:', echo=False)
         self.session = sessionmaker(bind=self.engine)()
         self.conn = self.engine.connect()
         Base.metadata.create_all(self.engine)
@@ -137,5 +57,5 @@ class SQLiteRepositry(Repository):
     def add(self, user):
         self.session.add(user)
 
-    def get(self, name):
-        return self.session.query(User).filter_by(first_name=name.first, last_name=name.last).first()
+    def get(self, index):
+        return self.session.query(User).filter(User.id == index).first()
