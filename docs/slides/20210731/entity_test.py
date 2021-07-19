f42e872be21d5d305e19b20d124e15dbe2361b32
k2works <kakimomokkuri@gmail.com>
2021-07-19 13:13:32 +0900

    refactor: エンティティ

--- docs/slides/20210731/entity_test.py
+++ docs/slides/20210731/entity_test.py
@@ -23,7 +23,7 @@ class TestUser(unittest.TestCase):
 
     def test_住所を登録できる(self):
         self.assertEqual(str(self.user.address), '722-001 広島県広島市横川町1-2-3 123')
-    
+
     def test_郵便局を登録できる(self):
         self.assertTrue(self.user.address.postal_code == PostalCode('722-001'))
 
@@ -38,17 +38,39 @@ class TestRepository(unittest.TestCase):
         name = Name(first='柿木', last='勝之')
         address = Address(postal_code=PostalCode('722-001'), prefecture='広島県',
                           city='広島市', town='横川町1-2-3', room='123')
-        self.user1 = User(name,address)
-        self.repo.add(self.user1)
+        self.user = User(name, address)
+        self.repo.add(self.user)
 
     def test_ユーザを登録できる(self):
         self.assertEqual(str(self.repo.get(1).name), '柿木 勝之')
-        self.assertEqual(self.repo.get(1).address.postal_code, PostalCode('722-001'))
+        self.assertEqual(self.repo.get(1).address.postal_code,
+                         PostalCode('722-001'))
+
+    def test_該当する条件のユーザを検索できる(self):
+        name = Name(first='柿木', last='カツオ')
+        address = Address(postal_code=PostalCode('163-8001'), prefecture='東京都',
+                          city='新宿区', town='西新宿2-8-1', room='')
+        user = User(name, address)
+        self.repo.add(user)
+
+        result = self.repo.select_by_address(address)
+        self.assertEqual(result.count(), 1)
+        self.assertEqual(result.first().name, name)
+        self.assertEqual(result.first().address, address)
+
+        result = self.repo.select_by_first_name(name)
+        self.assertEqual(result.count(), 2)
+        self.assertEqual(result.first().name.last, '勝之')
+        self.assertEqual(result[-1].name.last, 'カツオ')
+
+        result = self.repo.select_by_role_admin()
+        self.assertEqual(result.count(), 0)
 
 
 class Role(Enum):
     管理者 = 1
     利用者 = 2
+
 
 class PostalCode:
     def __init__(self, value):
@@ -56,6 +78,7 @@ class PostalCode:
 
     def __eq__(self, o: object) -> bool:
         return self.value == o.value
+
 
 class Address:
     def __init__(self, postal_code, prefecture, city, town, room):
@@ -68,6 +91,13 @@ class Address:
     def __str__(self) -> str:
         return '{} {}{}{} {}'.format(self.postal_code.value, self.prefecture, self.city, self.town, self.room)
 
+    def __eq__(self, o: object) -> bool:
+        return self.postal_code == o.postal_code and self.prefecture == o.prefecture and \
+               self.city == o.city and self.town == o.town and self.room == o.room
+
+    def __hash__(self) -> int:
+        return hash(self.postal_code)
+
 class Name:
     def __init__(self, first, last):
         self.first = first
@@ -75,6 +105,13 @@ class Name:
 
     def __str__(self) -> str:
         return '{} {}'.format(self.first, self.last)
+
+    def __eq__(self, o: object) -> bool:
+        return self.first == o.first and self.last == o.last
+
+    def __hash__(self) -> int:
+        return hash(self.first) + hash(self.last)
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -134,3 +171,20 @@ class SQLiteRepository(Repository):
 
     def get(self, index):
         return self.session.query(User).filter(User.id == index).first()
+
+    def select_by_address(self, address):
+        return self.session.query(User).filter(
+            User.postal_code == address.postal_code.value,
+            User.prefecture == address.prefecture,
+            User.city == address.city,
+            User.town == address.town,
+            User.room == address.room
+            )
+
+    def select_by_first_name(self, name):
+        return self.session.query(User).filter(
+            User.first_name == name.first,
+            )
+
+    def select_by_role_admin(self):
+        return self.session.query(User).filter(User.role_type == Role.管理者.value)
