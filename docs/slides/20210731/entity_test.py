39a11704b80311a6aceb1a232ec38dd7b46615e5
k2works <kakimomokkuri@gmail.com>
2021-07-24 14:37:58 +0900

    refactor: 関数の抽出

--- docs/slides/20210731/entity_test.py
+++ docs/slides/20210731/entity_test.py
@@ -1,15 +1,16 @@
 import unittest
 
 class TestUser(unittest.TestCase):
+    def setUp(self) -> None:
+        self.user = User()
+        self.user.name = "柿木 勝之"
+        self.user.address = "733-0011 広島県広島市西区横川町1-2-3 123"
+
     def test_名前を登録できる(self):
-        user = User()
-        user.name = "柿木 勝之"
-        self.assertEqual(user.name, "柿木 勝之")
+        self.assertEqual(self.user.name, "柿木 勝之")
 
     def test_住所を登録できる(self):
-        user = User()
-        user.address = "733-0011 広島県広島市西区横川町1-2-3 123"
-        self.assertEqual(user.address, "733-0011 広島県広島市西区横川町1-2-3 123")
+        self.assertEqual(self.user.address, "733-0011 広島県広島市西区横川町1-2-3 123")
 
 class User:
     pass
