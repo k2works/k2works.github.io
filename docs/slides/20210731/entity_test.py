e03279e751908ef43e1545531dc44b159b567507
k2works <kakimomokkuri@gmail.com>
2021-07-24 14:35:27 +0900

    test: エンティティ

--- docs/slides/20210731/entity_test.py
+++ docs/slides/20210731/entity_test.py
@@ -3,8 +3,13 @@ import unittest
 class TestUser(unittest.TestCase):
     def test_名前を登録できる(self):
         user = User()
-        user.name = "test"
-        self.assertEqual(user.name, "test")
+        user.name = "柿木 勝之"
+        self.assertEqual(user.name, "柿木 勝之")
+
+    def test_住所を登録できる(self):
+        user = User()
+        user.address = "733-0011 広島県広島市西区横川町1-2-3 123"
+        self.assertEqual(user.address, "733-0011 広島県広島市西区横川町1-2-3 123")
 
 class User:
     pass
