bd76047cc0e42d5c52be292c51e295e15f966073
k2works <kakimomokkuri@gmail.com>
2021-07-24 15:02:24 +0900

    test: エンティティ

--- docs/slides/20210731/entity_test.py
+++ docs/slides/20210731/entity_test.py
@@ -4,6 +4,7 @@ from sqlalchemy.engine import create_engine
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from enum import Enum
 
 Base = declarative_base()
 
@@ -12,6 +13,7 @@ class TestUser(unittest.TestCase):
         self.user = User()
         self.user.name = "柿木 勝之"
         self.user.address = "733-0011 広島県広島市西区横川町1-2-3 123"
+        self.user.role = Role.管理者
 
     def test_名前を登録できる(self):
         self.assertEqual(self.user.name, "柿木 勝之")
@@ -19,21 +21,32 @@ class TestUser(unittest.TestCase):
     def test_住所を登録できる(self):
         self.assertEqual(self.user.address, "733-0011 広島県広島市西区横川町1-2-3 123")
 
+    def test_権限を登録できる(self):
+        self.assertEqual(self.user.role, Role.管理者)
+
 class TestRepository(unittest.TestCase):
     def test_ユーザを登録できる(self):
         user = User()
         user.name = "柿木 勝之"
         user.address = "733-0011 広島県広島市西区横川町1-2-3 123"
+        user.role = Role.利用者.value
         repo = SQLiteRepository()
         repo.add_user(user)
 
         self.assertEqual(repo.get_user(1).name, "柿木 勝之")
+        self.assertEqual(repo.get_user(1).address, "733-0011 広島県広島市西区横川町1-2-3 123")
+        self.assertEqual(repo.get_user(1).role, Role.利用者.value)
+
+class Role(Enum):
+    管理者 = 1
+    利用者 = 2
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     name = Column(String)
     address = Column(String)
+    role = Column(Integer)
 
 
 class Repository:
