1dc2d3efd795c5eac4152cb887d42a4021534ab7
k2works <kakimomokkuri@gmail.com>
2022-12-13 17:03:51 +0900

    Create a table Code the add-in

--- docs/slides/20221217/My Office Add-in/src/taskpane/taskpane.js
+++ docs/slides/20221217/My Office Add-in/src/taskpane/taskpane.js
@@ -10,6 +10,15 @@ Office.onReady((info) => {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
+
+    // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
+    if (!Office.context.requirements.isSetSupported('ExcelApi', '1.7')) {
+        console.log('Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.');
+    }
+
+    // Assign event handlers and other initialization logic.
+    document.getElementById("create-table").onclick = createTable;
+
   }
 });
 
@@ -33,4 +42,38 @@ export async function run() {
   } catch (error) {
     console.error(error);
   }
+}
+
+async function createTable() {
+  await Excel.run(async (context) => {
+      // TODO1: Queue table creation logic here.
+      const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+      const expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
+      expensesTable.name = "ExpensesTable";
+
+      // TODO2: Queue commands to populate the table with data.
+      expensesTable.getHeaderRowRange().values = [["Date", "Merchant", "Category", "Amount"]];
+      expensesTable.rows.add(null /*add at the end*/, [
+        ["1/1/2017", "The Phone Company", "Communications", "120"],
+        ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
+        ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
+        ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
+        ["1/11/2017", "Bellows College", "Education", "350.1"],
+        ["1/15/2017", "Trey Research", "Other", "135"],
+        ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
+      ]);
+
+      // TODO3: Queue commands to format the table.
+      expensesTable.columns.getItemAt(3).getRange().numberFormat = [['\u20AC#,##0.00']];
+      expensesTable.getRange().format.autofitColumns();
+      expensesTable.getRange().format.autofitRows();
+
+      await context.sync();
+  })
+  .catch(function (error) {
+      console.log("Error: " + error);
+      if (error instanceof OfficeExtension.Error) {
+          console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      }
+  });
 }
