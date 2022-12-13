f03524093e0f5a8bd11172e3716db653b113d40a
k2works <kakimomokkuri@gmail.com>
2022-12-13 18:30:20 +0900

    Create a chart Chart a chart using table data

--- docs/slides/20221217/My Office Add-in/src/taskpane/taskpane.js
+++ docs/slides/20221217/My Office Add-in/src/taskpane/taskpane.js
@@ -19,6 +19,7 @@ Office.onReady((info) => {
     document.getElementById("create-table").onclick = createTable;
     document.getElementById("filter-table").onclick = filterTable;
     document.getElementById("sort-table").onclick = sortTable;
+    document.getElementById("create-chart").onclick = createChart;
   }
 });
 
@@ -86,6 +87,34 @@ async function sortTable() {
     ];
 
     expensesTable.sort.apply(sortFields);
+
+    await context.sync();
+  }).catch(function (error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+  });
+}
+
+async function createChart() {
+  await Excel.run(async (context) => {
+    // TODO1: Queue commands to get the range of data to be charted.
+    const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+    const expensesTable = currentWorksheet.tables.getItem("ExpensesTable");
+    const dataRange = expensesTable.getDataBodyRange();
+
+    // TODO2: Queue command to create the chart and define its type.
+    const chart = currentWorksheet.charts.add("ColumnClustered", dataRange, "Auto");
+
+    // TODO3: Queue commands to position and format the chart.
+    chart.setPosition("A15", "F30");
+    chart.title.text = "Expenses";
+    chart.legend.position = "Right";
+    chart.legend.format.fill.setSolidColor("white");
+    chart.dataLabels.format.font.size = 15;
+    chart.dataLabels.format.font.color = "black";
+    chart.series.getItemAt(0).name = "Value in \u20AC";
 
     await context.sync();
   }).catch(function (error) {
