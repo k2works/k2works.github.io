798d2285835e2d51e646427defe8c1125b55429d
k2works <kakimomokkuri@gmail.com>
2022-12-13 18:59:19 +0900

    Update webpack config settings

--- docs/slides/20221217/My Office Add-in/webpack.config.js
+++ docs/slides/20221217/My Office Add-in/webpack.config.js
@@ -20,6 +20,7 @@ module.exports = async (env, options) => {
       polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
       taskpane: "./src/taskpane/taskpane.js",
       commands: "./src/commands/commands.js",
+      popup: "./src/dialogs/popup.js",
     },
     output: {
       clean: true,
@@ -82,6 +83,11 @@ module.exports = async (env, options) => {
         filename: "commands.html",
         template: "./src/commands/commands.html",
         chunks: ["polyfill", "commands"],
+      }),
+      new HtmlWebpackPlugin({
+        filename: "popup.html",
+        template: "./src/dialogs/popup.html",
+        chunks: ["polyfill", "popup"],
       }),
     ],
     devServer: {
